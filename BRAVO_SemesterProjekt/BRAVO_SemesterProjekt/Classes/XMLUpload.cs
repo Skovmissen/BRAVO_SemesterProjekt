02dfa5641367c4b2e6a6bdc53b09925ca3f10de3
Skovmose <Skovmose@DESKTOP-V9IMISU>
2017-05-02 09:16:46 +0200

    xml

--- BRAVO_SemesterProjekt/BRAVO_SemesterProjekt/Classes/XMLUpload.cs
+++ BRAVO_SemesterProjekt/BRAVO_SemesterProjekt/Classes/XMLUpload.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace BRAVO_SemesterProjekt
 {
     class XMLUpload
     {
+        public static TempData ParseByXDocument()
+        {
+            var products = new TempData();
+
+            XmlDocument doc = new XmlDocument();
+           
+
+            return products;
+        }
     }
 }
